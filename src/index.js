8a3f8a23ad2219b00cff4164c719f14119857ec8
Ricky Reusser <rsreusser@gmail.com>
2017-08-28 17:13:33 -0700

    Make importing individual components more reasonable

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import PlotlyReactEditor from "./plotly.js-react-editor";
+
+import Field from "./components/field";
+import Select from "./components/select";
+import Panel from "./components/panel";
+import TraceAccordion from "./components/trace-accordion";
+
+PlotlyReactEditor.Field = Field;
+PlotlyReactEditor.Select = Select;
+PlotlyReactEditor.Panel = Panel;
+PlotlyReactEditor.TraceAccordion = TraceAccordion;
+
+export default PlotlyReactEditor;
