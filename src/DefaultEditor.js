612adddeaa9f9517a2c2907f3bad6113ef6826dd
bpostlethwaite <post.ben.here@gmail.com>
2017-10-20 16:53:27 -0400

    sort and prettify DefaultEditor

--- src/DefaultEditor.js
+++ src/DefaultEditor.js
@@ -1,19 +1,19 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { localize } from "./lib";
-
-import TraceAccordion from "./components/TraceAccordion";
-import Panel from "./components/Panel";
-import Numeric from "./components/Numeric";
-import ColorPicker from "./components/Color";
-import Section from "./components/Section";
-import Flaglist from "./components/Flaglist";
-import Radio from "./components/Radio";
-import PanelMenuWrapper from "./components/PanelMenuWrapper";
-import Dropdown from "./components/Dropdown";
+import ColorPicker from './components/Color';
+import Dropdown from './components/Dropdown';
+import Flaglist from './components/Flaglist';
+import Numeric from './components/Numeric';
+import Panel from './components/Panel';
+import PanelMenuWrapper from './components/PanelMenuWrapper';
+import PropTypes from 'prop-types';
+import Radio from './components/Radio';
+import React, {Component} from 'react';
+import Section from './components/Section';
+import TraceAccordion from './components/TraceAccordion';
+import {localize} from './lib';
 
 // These are the built-in panels for the editor. If the editor has children specified,
 // those panels will override these.
+
 class DefaultEditor extends Component {
   constructor(props, context) {
     super(props, context);
@@ -33,9 +33,9 @@ class DefaultEditor extends Component {
               clearable={false}
               attr="mode"
               options={[
-                { label: "Line", value: "lines" },
-                { label: "Scatter", value: "markers" },
-                { label: "Scatter line", value: "lines+markers" },
+                {label: 'Line', value: 'lines'},
+                {label: 'Scatter', value: 'markers'},
+                {label: 'Scatter line', value: 'lines+markers'}
               ]}
               show
             />
@@ -62,9 +62,9 @@ class DefaultEditor extends Component {
 
         <Panel section="Style" name="Traces">
           <TraceAccordion>
-            <Section heading={_("Trace")}>
+            <Section heading={_('Trace')}>
               <Numeric
-                label={_("Opacity")}
+                label={_('Opacity')}
                 min={0}
                 max={1}
                 step={0.1}
@@ -72,69 +72,69 @@ class DefaultEditor extends Component {
               />
             </Section>
 
-            <Section heading={_("Display")}>
+            <Section heading={_('Display')}>
               <Flaglist
                 attr="mode"
                 options={[
-                  { label: "Lines", value: "lines" },
-                  { label: "Points", value: "markers" },
+                  {label: 'Lines', value: 'lines'},
+                  {label: 'Points', value: 'markers'}
                 ]}
               />
             </Section>
 
-            <Section heading={_("Filled Area")}>
+            <Section heading={_('Filled Area')}>
               <Dropdown
                 label="Fill to"
                 attr="fill"
                 clearable={false}
                 options={[
-                  { label: "None", value: "none" },
-                  { label: "Y = 0", value: "tozeroy" },
-                  { label: "X = 0", value: "tozerox" },
-                  { label: "Previous Y", value: "tonexty" },
-                  { label: "Previous X", value: "tonextx" },
+                  {label: 'None', value: 'none'},
+                  {label: 'Y = 0', value: 'tozeroy'},
+                  {label: 'X = 0', value: 'tozerox'},
+                  {label: 'Previous Y', value: 'tonexty'},
+                  {label: 'Previous X', value: 'tonextx'}
                 ]}
               />
 
-              <ColorPicker label={_("Color")} attr="fillcolor" />
+              <ColorPicker label={_('Color')} attr="fillcolor" />
             </Section>
 
-            <Section heading={_("Points")}>
+            <Section heading={_('Points')}>
               <Numeric
-                label={_("Marker Opacity")}
+                label={_('Marker Opacity')}
                 min={0}
                 max={1}
                 step={0.1}
                 attr="marker.opacity"
               />
 
-              <ColorPicker label={_("Marker Color")} attr="marker.color" />
+              <ColorPicker label={_('Marker Color')} attr="marker.color" />
 
-              <Numeric label={_("Size")} min={0} attr="marker.size" />
+              <Numeric label={_('Size')} min={0} attr="marker.size" />
 
               <Numeric
-                label={_("Line width")}
+                label={_('Line width')}
                 min={0}
                 attr="marker.line.width"
               />
             </Section>
 
-            <Section heading={_("Lines")}>
+            <Section heading={_('Lines')}>
               <Numeric
-                label={_("Width")}
+                label={_('Width')}
                 min={0}
                 step={1.0}
                 attr="line.width"
               />
 
-              <ColorPicker label={_("Line color")} attr="line.color" />
+              <ColorPicker label={_('Line color')} attr="line.color" />
 
               <Radio
-                label={_("Connect Gaps")}
+                label={_('Connect Gaps')}
                 attr="connectgaps"
                 options={[
-                  { value: true, label: "Connect" },
-                  { value: false, label: "Blank" },
+                  {value: true, label: 'Connect'},
+                  {value: false, label: 'Blank'}
                 ]}
               />
             </Section>
@@ -147,7 +147,7 @@ class DefaultEditor extends Component {
 
 DefaultEditor.contextTypes = {
   dataSources: PropTypes.object,
-  dataSourceNames: PropTypes.array,
+  dataSourceNames: PropTypes.array
 };
 
 export default localize(DefaultEditor);
