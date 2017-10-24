12e09dbb39dbc9c22821c7bfcbb06bbccc04c518
bpostlethwaite <post.ben.here@gmail.com>
2017-10-24 10:51:08 -0400

    lint Panel.js

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,11 +1,11 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { bem } from "../lib";
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {bem} from '../lib';
 
 class Panel extends Component {
   render() {
     if (this.props.visible) {
-      return <div className={bem("panel")}>{this.props.children}</div>;
+      return <div className={bem('panel')}>{this.props.children}</div>;
     } else {
       return <div />;
     }
