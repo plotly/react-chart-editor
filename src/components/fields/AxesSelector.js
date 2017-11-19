b17632320468c87376728408e5547422da3c1b6a
bpostlethwaite <post.ben.here@gmail.com>
2017-11-18 21:28:42 -0500

    AxesSelector only handles context
    PR review comment suggested that until override via props is necessary
    we implement only the necessary context

--- src/components/fields/AxesSelector.js
+++ src/components/fields/AxesSelector.js
@@ -2,31 +2,21 @@ import Field from './Field';
 import PropTypes from 'prop-types';
 import RadioBlocks from '../widgets/RadioBlocks';
 import React, {Component} from 'react';
-import {connectToContainer} from '../../lib';
 
 export default class AxesSelector extends Component {
   constructor(props, context) {
     super(props, context);
 
-    if (!props.axesTargetHandler && !context.axesTargetHandler) {
+    if (!context.axesTargetHandler) {
       throw new Error(
-        'AxesSelector must be nested within a connectAxesToPlot component ' +
-          'or passed axesTargetHandler and axesOptions props'
+        'AxesSelector must be nested within a connectAxesToPlot component'
       );
     }
   }
 
   render() {
-    let axesTargetHandler, axesOptions, axesTarget;
-    if (this.props.axesTargetHandler) {
-      axesTargetHandler = this.props.axesTargetHandler;
-      axesOptions = this.props.axesOptions;
-      axesTarget = this.props.axesTarget;
-    } else {
-      axesTargetHandler = this.context.axesTargetHandler;
-      axesOptions = this.context.axesOptions;
-      axesTarget = this.context.axesTarget;
-    }
+    const {axesTargetHandler, axesOptions, axesTarget} = this.context;
+
     return (
       <Field {...this.props}>
         <RadioBlocks
@@ -38,12 +28,6 @@ export default class AxesSelector extends Component {
     );
   }
 }
-
-AxesSelector.propTypes = {
-  axesTargetHandler: PropTypes.func,
-  axesOptions: PropTypes.array,
-  axesTarget: PropTypes.string,
-};
 
 AxesSelector.contextTypes = {
   axesTargetHandler: PropTypes.func,
