9f553c4c759351d0c123f4b00ee8b311d4e49651
bpostlethwaite <post.ben.here@gmail.com>
2017-11-20 14:41:21 -0500

    use modify+supply funcs for BoxWidth and BoxPad

--- src/shame.js
+++ src/shame.js
@@ -1,6 +1,7 @@
 /*
  * DELETE THIS FILE. EVERYTHING NEEDS TO FIND A HOME.
  */
+import isNumeric from 'fast-isnumeric';
 import {list} from 'plotly.js/src/plots/cartesian/axis_ids';
 import {UnconnectedNumeric} from './components/fields/Numeric';
 import {
@@ -22,22 +23,67 @@ class NumericNoArrows extends UnconnectedNumeric {}
 NumericNoArrows.propTypes = UnconnectedNumeric.propTypes;
 NumericNoArrows.defaultProps = {
   showArrows: false,
+  postfix: '%',
 };
 
-export const BoxGap = connectLayoutToPlot(
+// Workaround the issue with nested layouts inside trace component.
+// See:
+// https://github.com/plotly/react-plotly.js-editor/issues/58#issuecomment-345492794
+const supplyLayoutPlotProps = (props, context) => {
+  return unpackPlotProps(props, {
+    ...context,
+    ...getLayoutContext(context),
+  });
+};
+
+export const BoxWidth = connectLayoutToPlot(
   connectToContainer(NumericNoArrows, {
-    supplyPlotProps: (props, context) => {
-      // workaround the issue with nested layouts inside trace component.
-      // See
-      // https://github.com/plotly/react-plotly.js-editor/issues/58#issuecomment-345492794
-      const plotProps = unpackPlotProps(props, {
-        ...context,
-        ...getLayoutContext(context),
-      });
+    supplyPlotProps: supplyLayoutPlotProps,
+    modifyPlotProps: (props, context, plotProps) => {
+      const {fullValue, updatePlot} = plotProps;
+      plotProps.fullValue = () => {
+        let fv = fullValue();
+        if (isNumeric(fv)) {
+          fv = Math.round((1 - fv) * 100);
+        }
+        return fv;
+      };
 
-      // Full value should multiply by percentage if number.
+      plotProps.updatePlot = v => {
+        if (isNumeric(v)) {
+          updatePlot(1 - v / 100);
+        } else {
+          updatePlot(v);
+        }
+      };
 
-      return plotProps;
+      plotProps.max = 100;
+    },
+  })
+);
+
+export const BoxPad = connectLayoutToPlot(
+  connectToContainer(NumericNoArrows, {
+    supplyPlotProps: supplyLayoutPlotProps,
+    modifyPlotProps: (props, context, plotProps) => {
+      const {fullValue, updatePlot} = plotProps;
+      plotProps.fullValue = () => {
+        let fv = fullValue();
+        if (isNumeric(fv)) {
+          fv = fv * 100;
+        }
+        return fv;
+      };
+
+      plotProps.updatePlot = v => {
+        if (isNumeric(v)) {
+          updatePlot(v / 100);
+        } else {
+          updatePlot(v);
+        }
+      };
+
+      plotProps.max = 100;
     },
   })
 );
