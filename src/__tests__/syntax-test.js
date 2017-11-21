971c716049c964c318264fae0ab7279cfc34c8d8
bpostlethwaite <post.ben.here@gmail.com>
2017-11-21 12:12:22 -0500

    add jasmine skip test blacklist meta test

---
+++ src/__tests__/syntax-test.js
@@ -0,0 +1,20 @@
+// check for for focus and exclude jasmine blocks
+import fs from 'fs';
+import glob from 'glob';
+
+const BLACK_LIST = ['fdescribe', 'fit', 'xdescribe', 'xit'];
+const REGEXS = BLACK_LIST.map(token => new RegExp(`^\\s*${token}\\(.*`));
+
+describe('Syntax and test validation', () => {
+  describe(`ensures ${BLACK_LIST} is not present in tests`, () => {
+    const files = glob.sync('**/__tests__/*.js');
+    files.forEach(file =>
+      it(`checks ${file} for test checks`, () => {
+        const code = fs.readFileSync(file, {encoding: 'utf-8'});
+        code.split('\n').forEach(line => {
+          expect(REGEXS.some(re => re.test(line))).toBe(false);
+        });
+      })
+    );
+  });
+});
