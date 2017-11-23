8783bc43b5813c591836401f10d56fba93a0f3f0
bpostlethwaite <post.ben.here@gmail.com>
2017-11-22 20:23:42 -0500

    Info no longer triggers visibility.

--- src/components/containers/__tests__/Section-test.js
+++ src/components/containers/__tests__/Section-test.js
@@ -50,14 +50,13 @@ describe('Section', () => {
     const wrapper = mount(
       <TestEditor onUpdate={jest.fn()} {...fixtures.scatter()}>
         <Section name="test-section">
-          <Info>INFO</Info>
+          <div className="extra">special extra</div>
         </Section>
       </TestEditor>
     ).find('[name="test-section"]');
 
     expect(wrapper.children().length).toBe(1);
-    expect(wrapper.find(Info).exists()).toBe(true);
-    expect(wrapper.find(Info).text()).toBe('INFO');
+    expect(wrapper.find('.extra').text()).toBe('special extra');
   });
 
   it('is not visible if it contains no visible children', () => {
@@ -102,7 +101,7 @@ describe('Section', () => {
     expect(wrapper.find(Info).text()).toBe('INFO');
   });
 
-  it('will hides even with MenuPanel when attrs not defined', () => {
+  it('will hide with MenuPanel children when attrs not defined', () => {
     const TraceSection = connectTraceToPlot(Section);
     const wrapper = mount(
       <TestEditor onUpdate={jest.fn()} {...fixtures.scatter()}>
@@ -111,14 +110,25 @@ describe('Section', () => {
           <MenuPanel show>
             <Info>INFO</Info>
           </MenuPanel>
-          <MenuPanel show>
-            <Info>MISINFORMATION</Info>
-          </MenuPanel>
         </TraceSection>
       </TestEditor>
     ).find('[name="test-section"]');
 
     expect(wrapper.find(MenuPanel).length).toBe(0);
+    expect(wrapper.find(Info).length).toBe(0);
+  });
+
+  it('will hide with Info children when attrs not defined', () => {
+    const TraceSection = connectTraceToPlot(Section);
+    const wrapper = mount(
+      <TestEditor onUpdate={jest.fn()} {...fixtures.scatter()}>
+        <TraceSection name="test-section" traceIndex={0}>
+          <Numeric attr="badattr" traceIndex={0} />
+          <Info>INFO</Info>
+        </TraceSection>
+      </TestEditor>
+    ).find('[name="test-section"]');
+
     expect(wrapper.find(Info).length).toBe(0);
   });
 });
