22ffcb66902ac5162a53f5967d3a77f2e6471b42
VeraZab <vera.zabeida@gmail.com>
2017-11-28 19:33:05 +0200

    Spacing

--- src/components/widgets/text_editors/RichText/debounce.js
+++ src/components/widgets/text_editors/RichText/debounce.js
@@ -2,20 +2,17 @@ const DEBOUNCE_DELAY = 250;
 let timeout;
 
 function clearTimeout() {
-    window.clearTimeout(timeout);
-    timeout = null;
+  window.clearTimeout(timeout);
+  timeout = null;
 }
 
 export default function debounce(fn, args) {
+  if (timeout) {
+    clearTimeout();
+  }
 
-    if (timeout) {
-        clearTimeout();
-    }
-
-    timeout = window.setTimeout(() => {
-
-        fn.apply(null, args);
-        timeout = null;
-
-    }, DEBOUNCE_DELAY);
+  timeout = window.setTimeout(() => {
+    fn.apply(null, args);
+    timeout = null;
+  }, DEBOUNCE_DELAY);
 }
