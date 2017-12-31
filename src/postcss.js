083232c91959fc80e7cb90264252d920968698cf
Thomas Osmonson <thomas.osmonson@gmail.com>
2017-12-31 14:41:07 -0600

    updates for theming, updated postcss settings

--- src/postcss.js
+++ src/postcss.js
@@ -3,33 +3,39 @@ const fs = require('fs');
 const postcss = require('postcss');
 const customProperties = require('postcss-custom-properties');
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const combineSelectors = require('postcss-combine-duplicated-selectors');
+const removeRoot = require('postcss-remove-root');
 
 /* eslint-disable no-process-env */
 const ie = process.env.NODE_ENV === 'ie';
 
 if (ie) {
-  fs.readFile('lib/react-plotly.js-editor.css', (err, css) => {
+  fs.readFile('dist/react-plotly.js-editor.css', (err, css) => {
     postcss([
+      combineSelectors,
       customProperties,
+      removeRoot,
       autoprefixer({browsers: ['last 5 versions', 'ie 11']}),
+      cssnano,
     ])
       .process(css, {
-        from: 'lib/react-plotly.js-editor.css',
-        to: 'lib/react-plotly.js-editor-ie11.css',
+        from: 'dist/react-plotly.js-editor.css',
+        to: 'dist/react-plotly.js-editor-ie11.min.css',
       })
       .then(result => {
-        fs.writeFile('lib/react-plotly.js-editor-ie11.css', result.css);
+        fs.writeFile('dist/react-plotly.js-editor-ie11.min.css', result.css);
       });
   });
 } else {
-  fs.readFile('lib/react-plotly.js-editor.css', (err, css) => {
-    postcss([autoprefixer])
+  fs.readFile('dist/react-plotly.js-editor.css', (err, css) => {
+    postcss([combineSelectors, autoprefixer, cssnano])
       .process(css, {
-        from: 'lib/react-plotly.js-editor.css',
-        to: 'lib/react-plotly.js-editor.css',
+        from: 'dist/react-plotly.js-editor.css',
+        to: 'dist/react-plotly.js-editor.css',
       })
       .then(result => {
-        fs.writeFile('lib/react-plotly.js-editor.css', result.css);
+        fs.writeFile('dist/react-plotly.js-editor.min.css', result.css);
       });
   });
 }
