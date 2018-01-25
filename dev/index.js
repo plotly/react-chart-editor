b770452d96c1874e9772e34e8eb4a835e0b6da87
bpostlethwaite <post.ben.here@gmail.com>
2018-01-25 15:49:13 -0500

    dev editor loads plotly mocks

--- dev/index.js
+++ dev/index.js
@@ -4,6 +4,7 @@ import plotly from 'plotly.js/dist/plotly';
 import createPlotComponent from 'react-plotly.js/factory';
 import PlotlyEditor from '../src';
 import '../src/styles/main.scss';
+import Nav from './Nav';
 
 const dataSources = {
   col1: [1, 2, 3], // eslint-disable-line no-magic-numbers
@@ -28,7 +29,19 @@ class App extends Component {
       graphDiv: {},
       editorRevision: 0,
       plotRevision: 0,
+      currentMockIndex: -1,
+      mocks: [],
     };
+
+    this.loadMock = this.loadMock.bind(this);
+  }
+
+  componentWillMount() {
+    fetch(
+      'https://api.github.com/repos/plotly/plotly.js/contents/test/image/mocks'
+    )
+      .then(response => response.json())
+      .then(mocks => this.setState({mocks}));
   }
 
   handlePlotUpdate(graphDiv) {
@@ -39,28 +52,52 @@ class App extends Component {
     this.setState(({plotRevision: x}) => ({plotRevision: x + 1}));
   }
 
+  loadMock(mockIndex) {
+    const mock = this.state.mocks[mockIndex];
+    fetch(mock.url, {
+      headers: new Headers({Accept: 'application/vnd.github.v3.raw'}),
+    })
+      .then(response => response.json())
+      .then(figure => {
+        const graphDiv = this.state.graphDiv;
+        graphDiv.layout = figure.layout;
+        graphDiv.data = figure.data;
+        this.setState(({plotRevision: x}) => ({
+          currentMockIndex: mockIndex,
+          plotRevision: x + 1,
+        }));
+      });
+  }
+
   render() {
     return (
-      <div className="app">
-        <PlotlyEditor
-          graphDiv={this.state.graphDiv}
-          onUpdate={this.handleEditorUpdate.bind(this)}
-          revision={this.state.editorRevision}
-          dataSources={dataSources}
-          dataSourceOptions={dataSourceOptions}
-          plotly={plotly}
+      <div>
+        <Nav
+          mocks={this.state.mocks}
+          currentMockIndex={this.state.currentMockIndex}
+          loadMock={this.loadMock}
         />
-        <div className="app__main" style={{width: '100%', height: '100%'}}>
-          <Plot
-            debug
-            useResizeHandler
-            data={this.state.graphDiv.data}
-            layout={this.state.graphDiv.layout}
-            onUpdate={this.handlePlotUpdate.bind(this)}
-            onInitialized={this.handlePlotUpdate.bind(this)}
-            revision={this.state.plotRevision}
-            style={{width: '100%', height: '100%', minHeight: '100vh'}}
+        <div className="app">
+          <PlotlyEditor
+            graphDiv={this.state.graphDiv}
+            onUpdate={this.handleEditorUpdate.bind(this)}
+            revision={this.state.editorRevision}
+            dataSources={dataSources}
+            dataSourceOptions={dataSourceOptions}
+            plotly={plotly}
           />
+          <div className="app__main" style={{width: '100%', height: '100%'}}>
+            <Plot
+              debug
+              useResizeHandler
+              data={this.state.graphDiv.data}
+              layout={this.state.graphDiv.layout}
+              onUpdate={this.handlePlotUpdate.bind(this)}
+              onInitialized={this.handlePlotUpdate.bind(this)}
+              revision={this.state.plotRevision}
+              style={{width: '100%', height: '100%', minHeight: '100vh'}}
+            />
+          </div>
         </div>
       </div>
     );
