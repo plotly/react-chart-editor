d46f0372872f3d3f4f4116ba65bb4c944d669460
VeraZab <vera.zabeida@gmail.com>
2018-01-26 23:10:21 -0500

    Timeseries trace added

--- src/lib/customTraceType.js
+++ src/lib/customTraceType.js
@@ -6,6 +6,8 @@ export function plotlyTraceToCustomTrace(trace) {
     )
   ) {
     return 'area';
+  } else if (trace.type === 'timeseries') {
+    return 'timeseries';
   } else if (
     trace.type === 'scatter' &&
     (trace.mode === 'lines' || trace.mode === 'lines+markers')
@@ -16,48 +18,46 @@ export function plotlyTraceToCustomTrace(trace) {
 }
 
 export function traceTypeToPlotlyInitFigure(traceType) {
-  if (traceType === 'line') {
-    return {type: 'scatter', mode: 'lines', fill: 'none'};
+  switch (traceType) {
+    case 'line':
+      return {type: 'scatter', mode: 'lines', fill: 'none'};
+    case 'scatter':
+      return {type: 'scatter', mode: 'markers', fill: 'none'};
+    case 'area':
+      return {type: 'scatter', fill: 'tozeroy'};
+    case 'ohlc':
+      return {
+        type: 'ohlc',
+        autobinx: true,
+        autobiny: true,
+        decreasing: {line: {color: '#7F7F7F'}},
+        increasing: {line: {color: '#17BECF'}},
+      };
+    case 'candlestick':
+      return {
+        type: 'candlestick',
+        decreasing: {
+          line: {color: '#7F7F7F'},
+          fillcolor: 'rgba(127, 127, 127, 0.5)',
+        },
+        increasing: {
+          line: {color: '#17BECF'},
+          fillcolor: 'rgba(23, 190, 207, 0.5)',
+        },
+      };
+    case 'box':
+      return {
+        type: 'box',
+        boxpoints: false,
+      };
+    case 'timeseries':
+      return {
+        type: 'timeseries',
+        mode: 'lines',
+        autobinx: true,
+        autobiny: true,
+      };
+    default:
+      return {type: traceType};
   }
-
-  if (traceType === 'scatter') {
-    return {type: 'scatter', mode: 'markers', fill: 'none'};
-  }
-
-  if (traceType === 'area') {
-    return {type: 'scatter', fill: 'tozeroy'};
-  }
-
-  if (traceType === 'ohlc') {
-    return {
-      type: 'ohlc',
-      autobinx: true,
-      autobiny: true,
-      decreasing: {line: {color: '#7F7F7F'}},
-      increasing: {line: {color: '#17BECF'}},
-    };
-  }
-
-  if (traceType === 'candlestick') {
-    return {
-      type: 'candlestick',
-      decreasing: {
-        line: {color: '#7F7F7F'},
-        fillcolor: 'rgba(127, 127, 127, 0.5)',
-      },
-      increasing: {
-        line: {color: '#17BECF'},
-        fillcolor: 'rgba(23, 190, 207, 0.5)',
-      },
-    };
-  }
-
-  if (traceType === 'box') {
-    return {
-      type: 'box',
-      boxpoints: false,
-    };
-  }
-
-  return {type: traceType};
 }
