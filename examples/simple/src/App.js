c91e145873e0010904572b4c3019c2bb9fb3dfe7
VeraZab <vera.zabeida@gmail.com>
2018-02-07 12:23:45 -0500

    Updating simple example

--- examples/simple/src/App.js
+++ examples/simple/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import plotly from 'plotly.js/dist/plotly-basic';
+import plotly from 'plotly.js/dist/plotly';
 import createPlotComponent from 'react-plotly.js/factory';
 import PlotlyEditor from 'react-plotly.js-editor';
 import 'react-plotly.js-editor/lib/react-plotly.js-editor.css';
+import Nav from './Nav';
 
 const dataSources = {
   col1: [1, 2, 3], // eslint-disable-line no-magic-numbers
@@ -27,7 +28,19 @@ class App extends Component {
       graphDiv: {},
       editorRevision: 0,
       plotRevision: 0,
+      currentMockIndex: -1,
+      mocks: [],
     };
+
+    this.loadMock = this.loadMock.bind(this);
+  }
+
+  componentWillMount() {
+    fetch(
+      'https://api.github.com/repos/plotly/plotly.js/contents/test/image/mocks'
+    )
+      .then(response => response.json())
+      .then(mocks => this.setState({mocks}));
   }
 
   handlePlotUpdate(graphDiv) {
@@ -38,29 +51,57 @@ class App extends Component {
     this.setState(({plotRevision: x}) => ({plotRevision: x + 1}));
   }
 
+  loadMock(mockIndex) {
+    const mock = this.state.mocks[mockIndex];
+    fetch(mock.url, {
+      headers: new Headers({Accept: 'application/vnd.github.v3.raw'}),
+    })
+      .then(response => response.json())
+      .then(figure => {
+        const graphDiv = this.state.graphDiv;
+        graphDiv.layout = figure.layout;
+        graphDiv.data = figure.data;
+        this.setState(({plotRevision: x}) => ({
+          currentMockIndex: mockIndex,
+          plotRevision: x + 1,
+        }));
+      });
+  }
+
   render() {
     return (
-      <div className="app">
-        <PlotlyEditor
-          graphDiv={this.state.graphDiv}
-          onUpdate={this.handleEditorUpdate.bind(this)}
-          revision={this.state.editorRevision}
-          dataSources={dataSources}
-          dataSourceOptions={dataSourceOptions}
-          plotly={plotly}
-        />
-        <div className="app__main" style={{width: '100%', height: '100%'}}>
-          <Plot
-            debug
-            useResizeHandler
-            data={this.state.graphDiv.data}
-            layout={this.state.graphDiv.layout}
-            onUpdate={this.handlePlotUpdate.bind(this)}
-            onInitialized={this.handlePlotUpdate.bind(this)}
-            revision={this.state.plotRevision}
-            style={{width: '100%', height: '100%'}}
+      <div className="app__container plotly-editor--theme-provider">
+        <div className="app">
+          <PlotlyEditor
+            graphDiv={this.state.graphDiv}
+            onUpdate={this.handleEditorUpdate.bind(this)}
+            revision={this.state.editorRevision}
+            dataSources={dataSources}
+            dataSourceOptions={dataSourceOptions}
+            plotly={plotly}
           />
+          <div className="app__main" style={{width: '100%', height: '100%'}}>
+            <Plot
+              data={this.state.graphDiv.data}
+              debug
+              layout={this.state.graphDiv.layout}
+              onInitialized={this.handlePlotUpdate.bind(this)}
+              onUpdate={this.handlePlotUpdate.bind(this)}
+              revision={this.state.plotRevision}
+              useResizeHandler
+              style={{
+                width: '100%',
+                height: '100%',
+                minHeight: 'calc(100vh - 50px)',
+              }}
+            />
+          </div>
         </div>
+        <Nav
+          currentMockIndex={this.state.currentMockIndex}
+          loadMock={this.loadMock}
+          mocks={this.state.mocks}
+        />
       </div>
     );
   }
---
+++ examples/simple/src/Nav.js
@@ -0,0 +1,35 @@
+import 'react-select/dist/react-select.css';
+import PropTypes from 'prop-types';
+import React from 'react';
+import Select from 'react-select';
+
+const Nav = props => (
+  <div className="mock-nav">
+    <span className="mock-nav__label">Select mock:</span>
+    <div className="mock-nav__select">
+      <Select
+        clearable={true}
+        value={props.currentMockIndex}
+        name="mock-dropdown"
+        options={props.mocks.map((item, i) => ({
+          label: item.name,
+          value: i,
+        }))}
+        className="open-top"
+        searchable={true}
+        searchPromptText="Search for a mock"
+        onChange={option => props.loadMock(option.value)}
+        noResultsText={'No Results'}
+        placeholder={'plotly.js/contents/test/image/mocks'}
+      />
+    </div>
+  </div>
+);
+
+Nav.propTypes = {
+  currentMockIndex: PropTypes.number,
+  loadMock: PropTypes.func,
+  mocks: PropTypes.array,
+};
+
+export default Nav;
