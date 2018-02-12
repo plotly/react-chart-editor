da91b611e7e5b5c183d11248a6afca417282fb6d
VeraZab <vera.zabeida@gmail.com>
2018-02-12 15:02:43 -0500

    Use RadioBlocks && correct controls

--- src/components/fields/ErrorBars.js
+++ src/components/fields/ErrorBars.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types';
 import React, {Component, Fragment} from 'react';
 import {DataSelector, Radio, Numeric} from '../index';
-import {UnconnectedRadio} from './Radio';
+import RadioBlocks from '../widgets/RadioBlocks';
+import Field from './Field';
 import {connectToContainer} from 'lib';
 
 class ErrorBars extends Component {
@@ -42,11 +43,24 @@ class ErrorBars extends Component {
       mode = 'hidden';
     }
 
-    if (this.props.fullValue.visible && this.props.fullValue.symmetric) {
+    if (
+      this.props.fullValue.visible &&
+      (this.props.fullValue.symmetric ||
+        typeof this.props.fullValue.symmetric === 'undefined')
+    ) {
+      // when this.props.fullValue.type === 'sqrt',
+      // then this.props.fullValue.symmetric is undefined, but 'sqrt' is only
+      // applicable when we want symmetric error bars
+      // https://github.com/plotly/plotly.js/issues/2359
       mode = 'symmetric';
     }
 
-    if (this.props.fullValue.visible && !this.props.fullValue.symmetric) {
+    if (
+      this.props.fullValue.visible &&
+      this.props.fullValue.symmetric === false
+    ) {
+      // it has to be explicitly set to false, because we don't want it to catch
+      // cases when it's undefined
       mode = 'asymmetric';
     }
 
@@ -57,36 +71,46 @@ class ErrorBars extends Component {
     const {localize: _} = this.props;
 
     return (
-      <UnconnectedRadio
-        label={_('Type')}
-        updatePlot={this.updatePlot}
-        activeOption={this.getMode()}
-        options={[
-          {label: _('Symmetric'), value: 'symmetric'},
-          {label: _('Asymmetric'), value: 'asymmetric'},
-          {label: _('Hidden'), value: 'hidden'},
-        ]}
-      />
+      <Field>
+        <RadioBlocks
+          alignment="center"
+          onOptionChange={this.updatePlot}
+          activeOption={this.getMode()}
+          options={[
+            {label: _('Symmetric'), value: 'symmetric'},
+            {label: _('Asymmetric'), value: 'asymmetric'},
+            {label: _('Hidden'), value: 'hidden'},
+          ]}
+        />
+      </Field>
     );
   }
 
   renderErrorBarControls() {
     const {localize: _} = this.props;
     const mode = this.getMode();
+    const showCustomDataControl = this.props.fullValue.type === 'data';
 
     if (mode === 'symmetric') {
       return (
         <Fragment>
           <Radio
-            label={_('Symmetric Error Type')}
+            label={_('Error Type')}
             attr={`${this.props.attr}.type`}
             options={[
               {label: _('%'), value: 'percent'},
               {label: _('Constant'), value: 'constant'},
               {label: _('√'), value: 'sqrt'},
+              {label: _('Data'), value: 'data'},
             ]}
           />
           <Numeric label={_('Value')} attr={`${this.props.attr}.value`} />
+          {showCustomDataControl ? (
+            <DataSelector
+              label={_(`Custom Data`)}
+              attr={`${this.props.attr}.array`}
+            />
+          ) : null}
         </Fragment>
       );
     }
@@ -94,14 +118,32 @@ class ErrorBars extends Component {
     if (mode === 'asymmetric') {
       return (
         <Fragment>
-          <DataSelector
-            label={_(`Error (+)`)}
-            attr={`${this.props.attr}.array`}
+          <Radio
+            label={_('Error Type')}
+            attr={`${this.props.attr}.type`}
+            options={[
+              {label: _('%'), value: 'percent'},
+              {label: _('Constant'), value: 'constant'},
+              {label: _('Data'), value: 'data'},
+            ]}
           />
-          <DataSelector
-            label={_(`Error (-)`)}
-            attr={`${this.props.attr}.arrayminus`}
+          <Numeric label={_('Value')} attr={`${this.props.attr}.value`} />
+          <Numeric
+            label={_('Value (-)')}
+            attr={`${this.props.attr}.valueminus`}
           />
+          {showCustomDataControl ? (
+            <Fragment>
+              <DataSelector
+                label={_(`Error (+)`)}
+                attr={`${this.props.attr}.array`}
+              />
+              <DataSelector
+                label={_(`Error (-)`)}
+                attr={`${this.props.attr}.arrayminus`}
+              />
+            </Fragment>
+          ) : null}
         </Fragment>
       );
     }
