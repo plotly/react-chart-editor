816d28259513323cc125ac949b8c32d578136f22
VeraZab <vera.zabeida@gmail.com>
2018-02-18 21:14:15 -0500

    Export getAllAxes and adjust for subplots

--- src/lib/index.js
+++ src/lib/index.js
@@ -10,6 +10,7 @@ import connectToContainer, {
 import connectTraceToPlot from './connectTraceToPlot';
 import dereference from './dereference';
 import findFullTraceIndex from './findFullTraceIndex';
+import getAllAxes from './getAllAxes';
 import localize, {localizeString} from './localize';
 import tinyColor from 'tinycolor2';
 import unpackPlotProps from './unpackPlotProps';
@@ -62,6 +63,7 @@ export {
   traceTypeToPlotlyInitFigure,
   dereference,
   findFullTraceIndex,
+  getAllAxes,
   getDisplayName,
   getLayoutContext,
   isPlainObject,
