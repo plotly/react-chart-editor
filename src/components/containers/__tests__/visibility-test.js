75f664951bd56ed5b7e4837ea3c0a9da2005b4ec
Nicolas Kruchten <nicolas@datacratic.com>
2018-03-09 13:58:21 -0500

    finishing up test

--- src/components/containers/__tests__/visibility-test.js
+++ src/components/containers/__tests__/visibility-test.js
@@ -3,6 +3,56 @@ import {LayoutPanel, Fold, Section} from '..';
 import React from 'react';
 import {TestEditor, fixtures, mount} from 'lib/test-utils';
 
+// Basic Section visibility rules (in a panel for context)
+describe('Panel > Section', () => {
+  describe('div', () => {
+    const wrapper = mount(
+      <TestEditor {...fixtures.scatter()}>
+        <LayoutPanel>
+          <Section>
+            <div id="thediv"> ok </div>
+          </Section>
+        </LayoutPanel>
+      </TestEditor>
+    );
+
+    it('shows Section', () =>
+      expect(wrapper.find('div.section').length).toEqual(1));
+    it('shows div', () => expect(wrapper.find('#thediv').length).toEqual(1));
+  });
+
+  describe('Field-with-visible-attr', () => {
+    const wrapper = mount(
+      <TestEditor {...fixtures.scatter()}>
+        <LayoutPanel>
+          <Section>
+            <Numeric attr="title" />
+          </Section>
+        </LayoutPanel>
+      </TestEditor>
+    );
+    it('shows Section', () =>
+      expect(wrapper.find('div.section').length).toEqual(1));
+    it('shows Field', () => expect(wrapper.find('input').length).toEqual(1));
+  });
+
+  describe('Field-with-invisible-attr', () => {
+    const wrapper = mount(
+      <TestEditor {...fixtures.scatter()}>
+        <LayoutPanel>
+          <Section>
+            <Numeric attr="not_an_attr" />
+          </Section>
+        </LayoutPanel>
+      </TestEditor>
+    );
+
+    it('HIDES Section', () =>
+      expect(wrapper.find('div.section').length).toEqual(0));
+    it('HIDES Field', () => expect(wrapper.find('input').length).toEqual(0));
+  });
+});
+
 // Basic Fold visibility rules
 describe('Panel > Fold', () => {
   describe('div', () => {
@@ -285,6 +335,7 @@ describe('Panel > Fold > Section-with-invisible-attr', () => {
   });
 });
 
+// nothing in the lower level Fold can force the upper one open
 describe('Panel > Fold > Panel > Fold', () => {
   describe('div', () => {
     const wrapper = mount(
@@ -342,6 +393,8 @@ describe('Panel > Fold > Panel > Fold', () => {
     it('HIDES Field', () => expect(wrapper.find('input').length).toEqual(0));
   });
 });
+
+// nothing in the lower level Fold can force the upper one open
 
 describe('Panel > Fold > Panel > Fold > Section', () => {
   describe('div', () => {
@@ -412,6 +465,9 @@ describe('Panel > Fold > Panel > Fold > Section', () => {
     it('HIDES Field', () => expect(wrapper.find('input').length).toEqual(0));
   });
 });
+
+// Lower level Panel with visible attr forces the upper Fold open, and visible
+// attr on Section forces the lower Fold open
 
 describe('Panel > Fold > Panel-with-visible-attr > Fold > Section-with-visible-attr', () => {
   describe('div', () => {
