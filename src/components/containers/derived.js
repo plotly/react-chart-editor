2fc20685862fdc62d4fc2116bcd40d1d4e44868b
VeraZab <vera.zabeida@gmail.com>
2018-03-21 11:13:10 -0400

    Make TraceTypeSection usable in connectedToLayout components

--- src/components/containers/derived.js
+++ src/components/containers/derived.js
@@ -8,12 +8,25 @@ import {connectLayoutToPlot, containerConnectedContextTypes} from 'lib';
 const LayoutPanel = connectLayoutToPlot(Panel);
 
 const TraceTypeSection = (props, context) => {
-  const {fullContainer} = context;
+  const {fullContainer, fullData} = context;
+  // when TraceTypeSection is used in a connectedToTrace context
   if (
     fullContainer &&
     ((fullContainer._fullInput &&
       props.traceTypes.includes(fullContainer._fullInput.type)) ||
       props.traceTypes.includes(fullContainer.type))
+  ) {
+    return <Section {...props} />;
+  }
+
+  // when TraceTypeSection is used in a connectedToLayout context
+  if (
+    fullData &&
+    fullData.some(
+      t =>
+        props.traceTypes.includes(t._fullInput.type) ||
+        fullData.some(t => props.traceTypes.includes(t.type))
+    )
   ) {
     return <Section {...props} />;
   }
