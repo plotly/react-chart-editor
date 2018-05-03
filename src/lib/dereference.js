8757bb171ee97e540012387283a4c87586cdbc59
VeraZab <vera.zabeida@gmail.com>
2018-05-03 11:14:18 -0400

    Add deleteKeys to config

--- src/lib/dereference.js
+++ src/lib/dereference.js
@@ -2,7 +2,7 @@ import walkObject from './walkObject';
 
 const SRC_ATTR_PATTERN = /src$/;
 
-export default function dereference(container, dataSources, deleteKeys) {
+export default function dereference(container, dataSources, config = {deleteKeys: false}) {
   const replacer = (key, parent) => {
     if (!SRC_ATTR_PATTERN.test(key)) {
       return;
@@ -12,7 +12,7 @@ export default function dereference(container, dataSources, deleteKeys) {
     const data = dataSources[srcRef];
     const dataKey = key.replace(SRC_ATTR_PATTERN, '');
 
-    if (deleteKeys && !(srcRef in dataSources)) {
+    if (config.deleteKeys && !(srcRef in dataSources)) {
       delete parent[dataKey];
       return;
     }
