93b6614d43ec2f6cc02b16578b104463d8559f7e
VeraZab <vera.zabeida@gmail.com>
2018-05-24 15:49:27 -0400

    Add tests

---
+++ src/lib/__tests__/maybeTransposeData-test.js
@@ -0,0 +1,44 @@
+import {maybeTransposeData} from '../index';
+/* eslint-disable no-magic-numbers */
+describe('maybeTransposeData', () => {
+  it('transposes 2d data for row based traceTypes', () => {
+    const transposed = maybeTransposeData(
+      [[1, 2, 3], [4, 5, 6]],
+      'zsrc',
+      'heatmap'
+    );
+
+    // [[1, 4], [2, 5], [3, 6]]
+    expect(transposed.length).toBe(3);
+  });
+
+  it('transposes 1d data for row based traceTypes', () => {
+    const transposed = maybeTransposeData([1, 2, 3], 'zsrc', 'heatmap');
+
+    // [[1], [2], [3]]
+    expect(transposed.length).toBe(3);
+  });
+
+  it('does not transpose data for column based traceTypes', () => {
+    const transposed = maybeTransposeData(
+      [[1, 2, 3], [4, 5, 6]],
+      'header.valuessrc',
+      'table'
+    );
+
+    // [[1, 2, 3], [4, 5, 6]]
+    expect(transposed.length).toBe(2);
+  });
+
+  it('removes extra array wrapping for special cases in tables', () => {
+    const transposed = maybeTransposeData(
+      [[1, 2, 3]],
+      'header.valuessrc',
+      'table'
+    );
+
+    // [1, 2, 3]
+    expect(Array.isArray(transposed[0])).toBe(false);
+    expect(transposed.length).toBe(3);
+  });
+});
