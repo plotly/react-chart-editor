a5182cb5d5bc6e3b3e67212378c7a7dc7dc75d1d
dmt0 <dmitry@plot.ly>
2018-06-15 15:48:10 -0400

    Rearrange operations list in filters; support comma-separated inclusion and exclusion lists

--- src/components/fields/FilterOperation.js
+++ src/components/fields/FilterOperation.js
@@ -11,34 +11,45 @@ const operators = [
     value: 'inequality',
   },
   {
-    label: 'Range',
-    value: 'range',
+    label: 'Include Range',
+    value: 'inrange',
   },
   {
-    label: 'Matching values',
-    value: 'set',
+    label: 'Exclude Range',
+    value: 'exrange',
+  },
+  {
+    label: 'Include Values',
+    value: 'inset',
+  },
+  {
+    label: 'Exclude Values',
+    value: 'exset',
   },
 ];
 
 const operations = {
   inequality: [
-    {value: '<', label: '<'},
-    {value: '<=', label: '≤'},
-    {value: '=', label: '='},
-    {value: '>', label: '>'},
-    {value: '>=', label: '≥'},
+    {value: '<', label: 'Target < Reference'},
+    {value: '<=', label: 'Target ≤ Reference'},
+    {value: '=', label: 'Target = Reference'},
+    {value: '>', label: 'Target > Reference'},
+    {value: '>=', label: 'Target ≥ Reference'},
   ],
-  range: [
-    {value: '[]', label: 'Lower ≤ x ≤ Upper'},
-    {value: '()', label: 'Lower < x < Upper'},
-    {value: '[)', label: 'Lower ≤ x < Upper'},
-    {value: '(]', label: 'Lower < x ≤ Upper'},
-    {value: '][', label: 'x ≤ Lower OR x ≥ Upper'},
-    {value: ')(', label: 'x < Lower OR x > Upper'},
-    {value: '](', label: 'x ≤ Lower OR x > Upper'},
-    {value: ')[', label: 'x < Lower OR x ≥ Upper'},
+  inrange: [
+    {value: '[]', label: 'Lower ≤ Target ≤ Upper'},
+    {value: '()', label: 'Lower < Target < Upper'},
+    {value: '[)', label: 'Lower ≤ Target < Upper'},
+    {value: '(]', label: 'Lower < Target ≤ Upper'},
   ],
-  set: [{value: '{}', label: 'Include'}, {value: '}{', label: 'Exclude'}],
+  exrange: [
+    {value: ')(', label: 'Lower ≤ Target ≤ Upper'},
+    {value: '][', label: 'Lower < Target < Upper'},
+    {value: ')[', label: 'Lower ≤ Target < Upper'},
+    {value: '](', label: 'Lower < Target ≤ Upper'},
+  ],
+  inset: [{value: '{}', label: 'Include'}],
+  exset: [{value: '}{', label: 'Exclude'}],
 };
 
 const findOperation = operator => {
@@ -85,7 +96,6 @@ class UnconnectedFilterOperation extends Component {
 
     const opValue =
       fullValue && fullValue.length > 0 ? fullValue : this.state.operator;
-
     return (
       <div>
         <Field {...this.props} attr={attr}>
@@ -98,15 +108,18 @@ class UnconnectedFilterOperation extends Component {
             optionRenderer={optionRenderer}
             valueRenderer={valueRenderer}
           />
-          <DropdownWidget
-            backgroundDark={backgroundDark}
-            options={operations[this.state.operation]}
-            value={opValue}
-            onChange={updatePlot}
-            clearable={false}
-            optionRenderer={optionRenderer}
-            valueRenderer={valueRenderer}
-          />
+          {this.state.operation === 'inset' ||
+          this.state.operation === 'exset' ? null : (
+            <DropdownWidget
+              backgroundDark={backgroundDark}
+              options={operations[this.state.operation]}
+              value={opValue}
+              onChange={updatePlot}
+              clearable={false}
+              optionRenderer={optionRenderer}
+              valueRenderer={valueRenderer}
+            />
+          )}
         </Field>
       </div>
     );
@@ -127,22 +140,17 @@ class UnconnectedFilterValue extends Component {
 
     this.setValue = this.setValue.bind(this);
     this.setValueMax = this.setValueMax.bind(this);
-    this.isRange = this.isRange.bind(this);
-  }
-
-  isRange() {
-    return (
-      this.context.container &&
-      this.context.container.operation &&
-      operations.range
-        .map(o => o.value)
-        .indexOf(this.context.container.operation) !== -1
-    );
   }
 
   setValue(v) {
+    const op = findOperation(this.context.container.operation);
     this.setState({value: v});
-    const val = this.isRange() ? [v, this.state.valueMax] : v;
+    let val;
+    val = op === 'inrange' || op === 'exrange' ? [v, this.state.valueMax] : v;
+    if (op === 'inset' || op === 'exset') {
+      val = val.split(',');
+      val = val.map(v => v.trim());
+    }
     this.props.updatePlot(val);
   }
 
@@ -152,22 +160,37 @@ class UnconnectedFilterValue extends Component {
   }
 
   render() {
-    const {localize: _} = this.context;
+    const {
+      localize: _,
+      container: {operation},
+    } = this.context;
     const {fullValue, attr, defaultValue} = this.props;
+    const op = findOperation(operation);
+
+    let label1 = _('Target');
+    if (op === 'inrange' || op === 'exrange') {
+      label1 = _('Lower Bound');
+    } else if (op === 'inset' || op === 'exset') {
+      label1 = _('Values');
+    }
+
+    let val1 = fullValue;
+    if ((op === 'inset' || op === 'exset') && Array.isArray(fullValue)) {
+      val1 = fullValue.join(', ');
+    } else if (Array.isArray(fullValue)) {
+      val1 = fullValue[0];
+    }
 
     return (
       <div>
-        <Field
-          {...this.props}
-          label={!this.isRange() ? _('Value') : _('Lower Bound')}
-        >
+        <Field {...this.props} label={label1}>
           <TextInput
-            value={Array.isArray(fullValue) ? fullValue[0] : fullValue}
-            defaultValue={defaultValue}
+            value={val1}
+            defaultValue={val1}
             onUpdate={this.setValue}
           />
         </Field>
-        {!this.isRange() ? null : (
+        {!(op === 'inrange' || op === 'exrange') ? null : (
           <Field {...this.props} label={_('Upper Bound')} attr={attr}>
             <TextInput
               value={Array.isArray(fullValue) ? fullValue[1] : fullValue}
