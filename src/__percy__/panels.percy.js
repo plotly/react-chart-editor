b2675b814983f02931a2ae1700c8739b3be2be79
dmt0 <dmitry@plot.ly>
2018-06-18 13:59:20 -0400

    percy initial

---
+++ src/__percy__/panels.percy.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {TestEditor, plotly, setupGraphDiv} from 'lib/test-utils';
+
+import {PanelMenuWrapper} from '../components';
+
+import * as mocks from '../../dev/percy';
+import * as panels from '../default_panels/';
+
+import '../../dev/styles.css';
+import '../styles/main.scss';
+import './percy.css';
+
+/**
+ * To add more Percy tests - add a mock file to /dev/percy, add it to /dev/percy/index.js
+ */
+
+const panelFixture = (Panel, group, name, data) => {
+  return (
+    <div className="plotly_editor">
+      <TestEditor plotly={plotly} graphDiv={setupGraphDiv(data)}>
+        <PanelMenuWrapper>
+          <Panel group={group} name={name} />
+        </PanelMenuWrapper>
+      </TestEditor>
+    </div>
+  );
+};
+
+const snapshotWidth = 500;
+
+Object.keys(mocks).forEach(m => {
+  Object.keys(panels).forEach(p => {
+    const words = p.split(/(?=[A-Z])/);
+    const panelGroup = words[0];
+    const panelName = words.slice(1, -1).join(' ');
+
+    percySnapshot(`${m}_${p}`, {widths: [snapshotWidth]}, () =>
+      panelFixture(panels[p], panelGroup, panelName, mocks[m])
+    );
+  });
+});
