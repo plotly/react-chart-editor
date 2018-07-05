f6768f6c024b6efd1211f62db8084d1286bed18f
dmt0 <dmitry@plot.ly>
2018-07-05 12:30:26 -0400

    Violin Percy test

--- dev/percy/index.js
+++ dev/percy/index.js
@@ -1,5 +1,6 @@
 import panelTest from './panelTest.json';
 import histogram from './histogram.json';
 import histogram2d from './histogram2d.json';
+import violin from './violin.json';
 
-export {panelTest, histogram, histogram2d};
+export {panelTest, histogram, histogram2d, violin};
