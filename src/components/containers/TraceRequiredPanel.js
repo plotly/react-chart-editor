5f08d6be3d988e4d312c6de1c0dedc25324beba8
dmt0 <dmitry@plot.ly>
2018-08-14 18:50:46 -0400

    bad merge fix

--- src/components/containers/TraceRequiredPanel.js
+++ src/components/containers/TraceRequiredPanel.js
@@ -11,48 +11,23 @@ class TraceRequiredPanel extends Component {
   render() {
     const {localize: _} = this.context;
     const {children, ...rest} = this.props;
-<<<<<<< HEAD
-=======
-    let showPanel = true;
-    let heading = '';
->>>>>>> e9befe5... add GraphCreatePanel link to empty panel
 
     if (!this.props.visible) {
       return null;
     }
 
-<<<<<<< HEAD
     return this.hasTrace() ? (
       <LayoutPanel {...rest}>{children}</LayoutPanel>
     ) : (
       <PanelEmpty
         heading={_("Looks like there aren't any traces defined yet.")}
       >
-=======
-    if (!this.hasTrace()) {
-      showPanel = false;
-      heading = _("Looks like there aren't any traces defined yet.");
-    }
-
-    return showPanel ? (
-      <LayoutPanel {...rest}>{children}</LayoutPanel>
-    ) : (
-      <PanelEmpty heading={heading}>
->>>>>>> e9befe5... add GraphCreatePanel link to empty panel
         <p>
           {_('Go to the ')}
           <a onClick={() => this.context.setPanel('Graph', 'Create')}>
             {_('Create')}
           </a>
-<<<<<<< HEAD
-<<<<<<< HEAD
           {_(' panel to define traces.')}
-=======
-          {_(' tab to define traces.')}
->>>>>>> e9befe5... add GraphCreatePanel link to empty panel
-=======
-          {_(' panel to define traces.')}
->>>>>>> b220435... add links to Subplots panel
         </p>
       </PanelEmpty>
     );
