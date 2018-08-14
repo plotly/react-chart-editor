cdc89e9b499626567d10212b738fccb34df1e246
Nicolas Kruchten <nicolas@datacratic.com>
2018-08-14 16:31:24 -0700

    fix menus crash on hide

--- src/default_panels/StyleUpdateMenusPanel.js
+++ src/default_panels/StyleUpdateMenusPanel.js
@@ -4,42 +4,44 @@ import {
   ColorPicker,
   FontSelector,
   Numeric,
-  Radio,
   PlotlySection,
   UpdateMenuAccordion,
   UpdateMenuButtons,
+  VisibilitySelect,
 } from '../components';
 
 const StyleUpdateMenusPanel = (props, {localize: _}) => (
   <UpdateMenuAccordion>
-    <Radio
+    <VisibilitySelect
       attr="visible"
       options={[
         {label: _('Show'), value: true},
         {label: _('Hide'), value: false},
       ]}
-    />
-    <PlotlySection name={_('Button Labels')}>
-      <UpdateMenuButtons attr="buttons" />
-    </PlotlySection>
-    <PlotlySection name={_('Background')}>
-      <ColorPicker label={_('Color')} attr="bgcolor" />
-    </PlotlySection>
-    <PlotlySection name={_('Font')}>
-      <FontSelector label={_('Typeface')} attr="font.family" />
-      <Numeric label={_('Size')} attr="font.size" />
-      <ColorPicker label={_('Color')} attr="font.color" />
-    </PlotlySection>
-    <PlotlySection name={_('Border')}>
-      <Numeric label={_('Width')} attr="borderwidth" />
-      <ColorPicker label={_('Color')} attr="bordercolor" />
-    </PlotlySection>
-    <PlotlySection name={_('Padding')}>
-      <Numeric label={_('Top')} attr="pad.t" units="px" />
-      <Numeric label={_('Bottom')} attr="pad.b" units="px" />
-      <Numeric label={_('Left')} attr="pad.l" units="px" />
-      <Numeric label={_('Right')} attr="pad.r" units="px" />
-    </PlotlySection>
+      showOn={true}
+    >
+      <PlotlySection name={_('Button Labels')}>
+        <UpdateMenuButtons attr="buttons" />
+      </PlotlySection>
+      <PlotlySection name={_('Background')}>
+        <ColorPicker label={_('Color')} attr="bgcolor" />
+      </PlotlySection>
+      <PlotlySection name={_('Font')}>
+        <FontSelector label={_('Typeface')} attr="font.family" />
+        <Numeric label={_('Size')} attr="font.size" />
+        <ColorPicker label={_('Color')} attr="font.color" />
+      </PlotlySection>
+      <PlotlySection name={_('Border')}>
+        <Numeric label={_('Width')} attr="borderwidth" />
+        <ColorPicker label={_('Color')} attr="bordercolor" />
+      </PlotlySection>
+      <PlotlySection name={_('Padding')}>
+        <Numeric label={_('Top')} attr="pad.t" units="px" />
+        <Numeric label={_('Bottom')} attr="pad.b" units="px" />
+        <Numeric label={_('Left')} attr="pad.l" units="px" />
+        <Numeric label={_('Right')} attr="pad.r" units="px" />
+      </PlotlySection>
+    </VisibilitySelect>
   </UpdateMenuAccordion>
 );
 
