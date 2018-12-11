96c5126475b3e548a7025d5e44509973054b1944
oprstchn <tyakachika@gmail.com>
2018-12-11 16:09:15 +0900

    remove context

--- src/context/index.js
+++ src/context/index.js
@@ -4,20 +4,7 @@ const PanelMenuWrapperContext = createContext({});
 const ModalProviderContext = createContext({});
 const PlotlyFoldContext = createContext({});
 const PlotlyPanelContext = createContext({});
-// const ConnectAggregationToTransformContext = createContext({});
-// const ConnectAnnotationToLayoutContext = createContext({});
-// const ConnectAxesToLayoutContext = createContext({});
-// const ConnectCartesianSubplotToLayoutContext = createContext({});
-// const ConnectImageToLayoutContext = createContext({});
-// const ConnectLayoutToPlotContext = createContext({});
-// const ConnectNonCartesianSubplotToLayoutContext = createContext({});
-// const ConnectRangeSelectorToAxisContext = createContext({});
-// const ConnectShapeToLayoutContext = createContext({});
-// const ConnectSliderToLayoutContext = createContext({});
-// const ConnectToContainerContext = createContext({});
 const ConnectTraceToPlotContext = createContext({});
-// const ConnectTransformToTraceContext = createContext({});
-// const ConnectUpdateMenuToLayoutContext = createContext({});
 
 export {
   EditorControlsContext,
@@ -25,18 +12,5 @@ export {
   ModalProviderContext,
   PlotlyFoldContext,
   PlotlyPanelContext,
-  // ConnectAggregationToTransformContext,
-  // ConnectAnnotationToLayoutContext,
-  // ConnectAxesToLayoutContext,
-  // ConnectCartesianSubplotToLayoutContext,
-  // ConnectImageToLayoutContext,
-  // ConnectLayoutToPlotContext,
-  // ConnectNonCartesianSubplotToLayoutContext,
-  // ConnectRangeSelectorToAxisContext,
-  // ConnectShapeToLayoutContext,
-  // ConnectSliderToLayoutContext,
-  // ConnectToContainerContext,
   ConnectTraceToPlotContext,
-  // ConnectTransformToTraceContext,
-  // ConnectUpdateMenuToLayoutContext,
 };
