33b8ce3a057bba3d530a00777f621f29da173c65
dmt0 <dmitry@plot.ly>
2019-09-25 15:21:05 -0400

    Storybook webpack return object is deprecated

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,14 +1,10 @@
 const path = require('path');
 
-module.exports = {
-  devtool: 'false',
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)?$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        include: path.resolve(__dirname, '../'),
-      },
-    ],
-  },
+module.exports = async ({config, mode}) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  });
+  return config;
 };
