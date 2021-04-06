8c46df467d625785e830462d7c9eaf864df696ca
dmt0 <dmitry@plot.ly>
2021-04-06 12:29:42 -0400

    Remove body padding added by v6

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import {configure} from '@storybook/react';
-
-function loadStories() {
-  require('../src/__stories__/index.js');
-  // You can require as many stories as you need.
-}
-
-configure(loadStories, module);
